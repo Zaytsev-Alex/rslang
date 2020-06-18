98af263eab5413a93ec22976b29198379dc1f0db
Denis <suboch.denny@gmail.com>
2020-06-18 16:26:14 +0300

    feat:  add additional info for prompts

--- src/js/MainContent/Cards/cardInfo.js
+++ src/js/MainContent/Cards/cardInfo.js
@@ -7,11 +7,12 @@ export default function addCardInfo() {
         img.classList.add('card__img');
        img.src = `https://raw.githubusercontent.com/icexes/rslang-data/master/${data[1].image}`;
         const textInput = document.querySelector('.card__input');
-        textInput.style.width = `${data[1].word.length * 16.8 + 14}px`
+        textInput.style.width = `${data[1].word.length * 16.8 + 18}px`
         document.querySelector('.card__image').append(img);
         document.querySelector('.card__word-translate').textContent = data[1].wordTranslate;
         document.querySelector('.card__transcription').textContent = data[1].transcription;
-        document.querySelector('.card__explain').textContent = data[1].textMeaningTranslate;
-        document.querySelector('.card__word-expample').textContent = data[1].textExampleTranslate;
-});
+        document.querySelector('.card__explain').innerHTML = `Значение: ${data[1].textMeaning}`;
+        document.querySelector('.card__word-expample').innerHTML = `Пример: ${ data[1].textExample}`;
+})
+
 }
