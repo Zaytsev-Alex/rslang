fbc6310c28273fdf89796410b1c81b7008116e46
DmiKol <dmikol13@mail.ru>
2020-06-22 15:39:32 +0300

    feat: add start srceen

--- src/script.js
+++ src/script.js
@@ -4,4 +4,20 @@ window.onload = () => {
     mainBlock.classList.add('main');
     document.body.append(mainBlock);
 
+    const startScreen = document.createElement('div');
+    startScreen.classList.add('start-screen');
+    mainBlock.append(startScreen);
+
+    const titleGame = document.createElement('h1');
+    startScreen.append(titleGame);
+    titleGame.innerText = "Audio Call";
+
+    const startBtn = document.createElement('div');
+    startBtn.classList.add('start-btn');
+    startScreen.append(startBtn);
+    startBtn.innerText = "start";
+
+
+
+
 }
