69d0ca38852d19e6188bfb254628dbc7a7e40d8b
ROnic404 <ronic@inbox.ru>
2020-06-23 19:44:22 +0400

    refactor: change generate HTML

--- src/js/authorization/generateHTML.js
+++ src/js/authorization/generateHTML.js
@@ -1,26 +1,34 @@
-const MAIN_TAG = document.querySelector('main');
-const HTMLTemplate = document.createElement('section');
+const HTMLTemplate = document.createElement('div');
 
-HTMLTemplate.className = 'login-page';
-HTMLTemplate.innerHTML = `
-<div class="form-block">
-  <form action="users" method="POST" class="registration hide" id="registration-form">
-    <h2 class="registration__title">Регистрация</h2>
-    <input type="email" name="email" id="registration-email" placeholder="email" autocomplete="off" required>
-    <p class="error registration__error" id="email-error"></p>
-    <input type="password" name="password" id="registration-password" placeholder="password" autocomplete="off" required>
-    <p class="error registration__error" id="password-error"></p>
-    <button class="button registration__button" id="registration__button" type="submit">Создать аккаунт</button>
-    <p class="registration__switch" id="have-account">У меня уже есть аккаунт</p>
-  </form>
-  <form action="#" method="POST" class="authorization hide" id="authorization-form">
-    <h2 class="authorization__title">Войти</h2>
-    <input type="email" name="email" id="authorization-email" placeholder="email" autocomplete="off" required>
-    <input type="password" name="password" id="authorization-password" placeholder="password" autocomplete="off" required>
-    <p class="error authorization__error" id="authorization-error"></p>
-    <button class="button authorization__button" id="authorization__button" type="submit">Войти</button>
-    <p class="registration__switch" id="create-account">Создать аккаунт</p>
-  </form>
-</div>`;
+HTMLTemplate.innerHTML = `<header class="header wrapper">
+<div class="logo">RS lang</div>
+<div class="header__buttons">
+  <button type="button" class="button header__button hide" id="log-out-button">Выйти</button>
+</div>
+</header>
 
-MAIN_TAG.append(HTMLTemplate);
+<main class="wrapper">
+<section class="login-page">
+  <div class="form-block">
+    <form action="users" method="POST" class="registration hide" id="registration-form">
+      <h2 class="registration__title">Регистрация</h2>
+      <input type="email" name="email" id="registration-email" placeholder="email" autocomplete="off" required>
+      <p class="error registration__error" id="email-error"></p>
+      <input type="password" name="password" id="registration-password" placeholder="password" autocomplete="off" required>
+      <p class="error registration__error" id="password-error"></p>
+      <button class="button registration__button" id="registration__button" type="submit">Создать аккаунт</button>
+      <p class="registration__switch" id="have-account">У меня уже есть аккаунт</p>
+    </form>
+    <form action="#" method="POST" class="authorization hide" id="authorization-form">
+      <h2 class="authorization__title">Войти</h2>
+      <input type="email" name="email" id="authorization-email" placeholder="email" autocomplete="off" required>
+      <input type="password" name="password" id="authorization-password" placeholder="password" autocomplete="off" required>
+      <p class="error authorization__error" id="authorization-error"></p>
+      <button class="button authorization__button" id="authorization__button" type="submit">Войти</button>
+      <p class="registration__switch" id="create-account">Создать аккаунт</p>
+    </form>
+  </div>
+</section>
+</main>`;
+
+document.body.append(HTMLTemplate);
