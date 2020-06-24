979b0ae4d4ec01dafe7e7e124b69b96df8a64863
Icexes <suboch.denny@gmail.com>
2020-06-24 23:10:15 +0300

    refactor: delete useless files

--- src/js/MainContent/Speech/SpeechRecognition.js
+++
@@ -1,7 +0,0 @@
-export default function createSpeachRecognition (lang) {
-    window.SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition;
-    const recognition = new window.SpeechRecognition();
-    recognition.lang = lang;
-    recognition.interimResults = false;
-    return recognition;
-    }
