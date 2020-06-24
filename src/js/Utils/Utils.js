2a2f115d8a96ee94df7fadfdfedf4eba3edcf362
Icexes <suboch.denny@gmail.com>
2020-06-24 23:14:45 +0300

    feat: add new functions

--- src/js/Utils/Utils.js
+++ src/js/Utils/Utils.js
@@ -1,11 +1,17 @@
-
-
+import {
+    WORD_TRANSLATE,
+    WORD_TRANSCRIPTION,
+    WORD_EXPLAIN,
+    WORD_EXAMPLE,
+    INPUT_FIELD,
+    IMAGE_CONTAINER
+} from '../Variables/variables'
 
 export function addTranslate(textExampleTranslate, textMeaningTranslate) {
     document.querySelector('.card__word-example-translate').textContent = textExampleTranslate;
     document.querySelector('.card__explain-translate').textContent = textMeaningTranslate;
 }
-  
+
 export function playAudio(audioSrc) {
     const src = `https://raw.githubusercontent.com/icexes/rslang-data/master/${audioSrc}`;
     const audio = new Audio();
@@ -16,14 +22,26 @@ export function playAudio(audioSrc) {
     return audio;
 }
 
-export const createProgressField = () => {
-    const progressField = document.createElement('div');
-    progressField.classList.add('progress');
-    progressField.innerHTML = `
-    <div class = 'progress__start-value'></div>
-    <div class = 'progress__row'>
-    <div class = 'progress__current'></div>
-    </div>
-    <div class = 'progress__end-value'></div> `;
-    return progressField;
+
+
+export function addCardInfo(data) {
+
+    const img = document.querySelector('.card__img') || document.createElement('img');
+    img.classList.add('card__img');
+    img.src = `https://raw.githubusercontent.com/icexes/rslang-data/master/${data.image}`;
+    INPUT_FIELD.style.width = `${data.word.length*16 + 24}px`;
+    IMAGE_CONTAINER.append(img);
+    WORD_TRANSLATE.textContent = data.wordTranslate;
+    WORD_TRANSCRIPTION.textContent = data.transcription;
+    WORD_EXPLAIN.innerHTML = `Значение: ${data.textMeaning.replace(/<i>\w+<\/i>/g, '...')}`;
+    WORD_EXAMPLE.innerHTML = `Пример: ${ data.textExample.replace(/<b>\w+<\/b>/g, '...')}`;
+
 }
+
+export function createSpeachRecognition (lang) {
+    window.SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition;
+    const recognition = new window.SpeechRecognition();
+    recognition.lang = lang;
+    recognition.interimResults = false;
+    return recognition;
+    }
