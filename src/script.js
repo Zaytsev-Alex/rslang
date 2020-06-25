03ba5044b6abd8bbdd9c03f074cb78a6c56cb19c
DmiKol <dmikol13@mail.ru>
2020-06-25 12:03:04 +0300

    feat: all without level

--- src/script.js
+++ src/script.js
@@ -17,6 +17,11 @@ window.onload = () => {
     startScreen.append(startBtn);
     startBtn.innerText = "start";
 
+    let countClick = 0;
+    let indWidth = 20;
+    let rightAnswer = 0;
+    let wrongAnswer = 0;
+
     let level = 0;
 
     let startGame = function(){
@@ -24,12 +29,13 @@ window.onload = () => {
         mainBlock.prepend(indicator);
         indicator.classList.add('indicator');
 
+        let nextGame = function (){
+
         startScreen.innerHTML = "";
         const soundImg = document.createElement('div');
         soundImg.classList.add('sound-img');
         startScreen.append(soundImg);
 
-        
         let page = Math.floor(0 + Math.random() * (29 + 1 - 0));
         
 
@@ -66,8 +72,19 @@ window.onload = () => {
                
             }
 
+            const result = document.createElement('div');
+            result.classList.add('result');
+            result.innerText = `Right ${rightAnswer}/${wrongAnswer} Wrong`;
+            startScreen.append(result);
+
+            const dontKnownBtn = document.createElement('div');
+            dontKnownBtn.classList.add('dont-known-btn');
+            startScreen.append(dontKnownBtn);
+            dontKnownBtn.innerText = "don't known";
+
             words.addEventListener('click', (event) => {
-                let num = event.target.innerText[0];
+                if (countClick === 0){
+                    let num = event.target.innerText[0];
                 for(let m = 0; m < 4; m += 1){
                     document.getElementsByClassName('opacity-no')[m].style.opacity = "0.3";
                 }
@@ -76,9 +93,11 @@ window.onload = () => {
                     const right = document.createElement('img');
                     right.src = "src/img/right.png";
                     event.target.prepend(right);
+                    rightAnswer += 1;
                     
                 }else{
                     event.target.style.textDecoration = "line-through";
+                    wrongAnswer += 1;
                 }
                 const engWord = document.createElement('p');
                 engWord.classList.add('eng-word');
@@ -93,46 +112,92 @@ window.onload = () => {
                 engWord.innerText = `${data[randWords+randWord-1].word}`;
                 soundImg.after(engWord);
                 soundImg.after(engImg);
+
+                countClick = 1;
+
+                dontKnownBtn.innerText = "";
+                dontKnownBtn.style.background = "url('src/img/arrow.png') no-repeat";
+                dontKnownBtn.style.backgroundPosition = "center";
+
+                }
+                
                  });
 
-            const dontKnownBtn = document.createElement('div');
-            dontKnownBtn.classList.add('dont-known-btn');
-            startScreen.append(dontKnownBtn);
-            dontKnownBtn.innerText = "don't known";
+            
 
             let clickDontKnown = function (){
-                for(let m = 0; m < 4; m += 1){
-                    document.getElementsByClassName('opacity-no')[m].style.opacity = "0.3";
+                if (countClick === 0){
+                    for(let m = 0; m < 4; m += 1){
+                        document.getElementsByClassName('opacity-no')[m].style.opacity = "0.3";
+                    }
+                    const right = document.createElement('img');
+                    right.src = "src/img/right.png";
+                    let dontWord = document.getElementsByClassName('opacity-on')[0];
+                    dontWord.innerText = dontWord.innerText.slice(2);
+                    dontWord.prepend(right);
+    
+    
+    
+                    const engWord = document.createElement('p');
+                    engWord.classList.add('eng-word');
+                    const engImg = document.createElement('img');
+                    engImg.classList.add('eng-img');
+                    
+                    const url2 = `https://dictionary.skyeng.ru/api/public/v1/words/search?search=${data[randWords+randWord-1].word}`;
+                    fetch(url2)
+                        .then((res) => res.json())
+                        .then((data) => {
+                            engImg.src = `${data[0].meanings[0].imageUrl}`;
+                       
+                        });
+                    engWord.innerText = `${data[randWords+randWord-1].word}`;
+                    soundImg.after(engWord);
+                    soundImg.after(engImg);
+
+                    countClick = 1;
+                    wrongAnswer += 1;
+
+                    dontKnownBtn.innerText = "";
+                    dontKnownBtn.style.background = "url('src/img/arrow.png') no-repeat";
+                    dontKnownBtn.style.backgroundPosition = "center";
+
+                }else{
+                    if (indicator.style.width === "100%"){
+                        dontKnownBtn.style.background = "none";
+                        startScreen.innerHTML = "";
+                        result.innerText = `Your result
+                        Right ${rightAnswer}/${wrongAnswer} Wrong`;
+                        startScreen.append(result);
+                        startScreen.append(dontKnownBtn);
+                        dontKnownBtn.innerText = "restart";
+                        dontKnownBtn.addEventListener('click', () => {
+                             countClick = 0;
+                             indicator.style.width = '10%';
+                             rightAnswer = 0;
+                             wrongAnswer = 0;
+                             indWidth = 20;
+                             startGame();
+                        });
+                        
+                    }else{
+                        startScreen.innerHTML = "";
+                        countClick = 0;
+                        indicator.style.width = `${indWidth}%`;
+                        indWidth += 10;
+                        nextGame();
+                    }
+                    
                 }
-                const right = document.createElement('img');
-                right.src = "src/img/right.png";
-                let dontWord = document.getElementsByClassName('opacity-on')[0];
-                dontWord.innerText = dontWord.innerText.slice(2);
-                dontWord.prepend(right);
-
-
-
-                const engWord = document.createElement('p');
-                engWord.classList.add('eng-word');
-                const engImg = document.createElement('img');
-                engImg.classList.add('eng-img');
-                
-                const url2 = `https://dictionary.skyeng.ru/api/public/v1/words/search?search=${data[randWords+randWord-1].word}`;
-                fetch(url2)
-                    .then((res) => res.json())
-                    .then((data) => {
-                        engImg.src = `${data[0].meanings[0].imageUrl}`;
-                   
-                    });
-                engWord.innerText = `${data[randWords+randWord-1].word}`;
-                soundImg.after(engWord);
-                soundImg.after(engImg);
                 
             }
 
             dontKnownBtn.addEventListener('click', clickDontKnown);
 
         });
+
+     }
+
+     nextGame();
 
         
     }
