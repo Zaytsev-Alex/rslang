36d6aab5095d319ba65dcbda270c14a2c48bfad0
staskorotkiy92 <staskorotkiy92@gmail.com>
2020-07-02 22:13:27 +0300

    feat: add clearHtml function

--- src/js/authorization/authorization.js
+++ src/js/authorization/authorization.js
@@ -5,8 +5,12 @@ import generateHtml from './generateHTML';
 import insertCreateUserCode from './createUser';
 import insertLoginUserCode from './loginUser';
 
+function clearHtml() {
+  document.body.innerHTML = '';
+}
 
 export default function authorization() {
+  clearHtml();
   generateHtml();
   insertCreateUserCode();
   insertLoginUserCode();
