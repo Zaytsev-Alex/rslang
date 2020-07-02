6c6ef4a04bae861440b0047da2324381b2b3be37
staskorotkiy92 <staskorotkiy92@gmail.com>
2020-07-02 22:10:02 +0300

    feat: add basic events for active elements and change import of functions which invoke by events

--- src/js/index.js
+++ src/js/index.js
@@ -3,14 +3,13 @@ import '../scss/intro_page/style.scss';
 
 import '../scss/authorization_page/style.scss';
 
-import menuButtonHandler from './components/nav-menu';
+ import menuButtonHandler from './components/nav-menu';
 
-import  menuButtonEvent from './events/events';
-
-import authorization from './authorization/authorization';
+import  {menuButtonEvent, events} from './events/events';
 
 menuButtonEvent(menuButtonHandler);
 
+events();
 
-document.querySelector('.start-learning__link').addEventListener('click', authorization);
+
 
