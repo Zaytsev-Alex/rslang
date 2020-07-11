abe3f80fbe880f808c6e37c53f32212252e0196e
staskorotkiy92 <staskorotkiy92@gmail.com>
2020-07-11 17:21:01 +0300

    feat: implement introPageRender function and add to logout event

--- src/js/main-page/basicHeader.js
+++ src/js/main-page/basicHeader.js
@@ -2,6 +2,7 @@ import showMainPage from './showMainPage';
 import clearContainer from '../clear';
 import showSettings from '../settings/showSettings';
 import showStatistics from '../statistics/showStatistics';
+import introPageRender from '../intro-page/intro-page-rendering';
 
 const setActiveStatus = (link) => {
     const activeItem = document.querySelector('.basic-header__item-active');
@@ -49,7 +50,8 @@ const basicHeaderHolder = () => {
         localStorage.removeItem('token');
         localStorage.removeItem('userId');
         clearContainer(document.body); 
-        console.log('Выход, надо показать интро страницу');
+        introPageRender();
+        // console.log('Выход, надо показать интро страницу');
     })
 }
 
